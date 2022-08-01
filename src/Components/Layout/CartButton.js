51512aea832464ec9aed8e2f55e9e8299d62e6fa
Avinash <rameshwarammlr@gmail.com>
2022-08-01 14:12:26 +0530

    adding items to cart

--- src/Components/Layout/CartButton.js
+++ src/Components/Layout/CartButton.js
@@ -1,14 +1,21 @@
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../Store/Cart-context";
 import style from "../Layout/CartButton.module.css";
+import { useContext } from "react";
 
 const CartButton = (props) => {
+  const context = useContext(CartContext);
+
+  const numberOfItem = context.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
   return (
     <button className={style.button} onClick={props.onClick}>
       <span className={style.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={style.badge}>3</span>
+      <span className={style.badge}>{numberOfItem}</span>
     </button>
   );
 };
---
+++ src/Components/Store/CartProvider.js
@@ -0,0 +1,40 @@
+import { useReducer } from "react";
+import CartContext from "./Cart-context";
+
+const defaultCartState = {
+    items : []
+    totalAmount :0
+}
+
+const cartReducer = (state, action) => {
+    if(action.type === "Add_item"){
+        const updateItems = state.items.concat(action.item);
+        const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount
+    }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer,defaultCartState)
+  
+    const addItemHandler = (item) => {
+        dispatchCartAction({type : "Add_item", item :item})
+    };
+
+  const removeItemhander = (id) => {
+    dispatchCartAction({type : "Remove_item", id :id})
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemHandler,
+    removeItem: removeItemhander,
+  };
+
+  <CartContext.Provider value={cartContext}>
+    {props.children}
+  </CartContext.Provider>;
+};
+
+export default CartProvider;
